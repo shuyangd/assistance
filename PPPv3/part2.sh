ddcbcb695fcb3e8764584130c221a4b8e6420192
shuyangd <shuyangd@csil-cpu2.cs.surrey.sfu.ca>
2016-10-15 14:23:45 -0700

    asdgf

---
+++ PPPv3/part2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+for ((i=3;i<10;i++));
+do
+   let G1=10**${i}
+   let G2=($G1)*5
+   #echo $G1
+   ./driver-sort --trials 5 --particles 40000000 --grainsize $G1 --threads 4
+   ./driver-sort --trials 5 --particles 40000000 --grainsize $G2 --threads 4
+done
